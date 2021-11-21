9af87cf442987235e7ed477f7d27a30db5f24932
JaibaH <jaredhernandezsilva@gmail.com>
2021-11-21 00:58:13 -0600

    Puse mi código de useContext

---
+++ react-context-users/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import UsersList from "./components/UsersList";
+import Profile from "./components/Profile";
+
+import UserState from "./context/User/UserState";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <UserState>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md-7">
+            <UsersList />
+          </div>
+          <div className="col-md-5">
+            <Profile />
+          </div>
+        </div>
+      </div>
+    </UserState>
+  );
+}
+
+export default App;
